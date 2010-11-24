918436b12bb2e13aab65d6c659e3f2d496249273
David Lee <davidlee@inbox.com>
2010-11-25 07:15:54 +0800

    quick sort

---
+++ chapter04/quicksort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/* qsort: sort v[left]...v[right] into increasing order */
+
+void qsort(int v[], int left, int right)
+{
+
+	int i, last;
+	void swap(int v[], int i, int j);
+	void printarr(int v[]);
+
+	if(left >= right)
+		return; /* do nothing if array contains fewer than two elements. */
+
+	swap(v, left, (left + right) / 2); /* move partition elem to v[0] */
+	last = left;
+	for(i = left + 1; i <= right; i++)
+		if(v[i] < v[left])
+		{
+			swap(v, ++last, i);
+			printarr(v);
+		}
+	swap(v, left, last); /* restore partition elelm */
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+
+void swap(int v[], int i, int j)
+{
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+void printarr(int v[])
+{
+	int i = 0;
+	while(v[i++])
+		printf("%3d ", v[i-1]);
+	printf("\n");
+}
+int main(void)
+{
+	int i;
+	int v[] = {23, 5, 10, 78, 28, 37, 67, 19, 5, 98, 32, 46};
+	qsort(v, 0, 11);
+	for(i = 0; i < 12; i++)
+		printf("%3d ", v[i]);
+	printf("\n");
+
+	return 0;
+}
